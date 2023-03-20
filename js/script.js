5321b352bc7abd9679659938716f5a8d7eb06773
Neordan <83787849+Neordan@users.noreply.github.com>
2023-03-20 01:13:37 +0100

    1er commit html + css + responsive + javascript

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// let list = document.querySelectorAll('.nav li');
+
+// function active() {
+//     list.forEach((i) =>
+//     i.classList.remove('active'));
+//     this.classList.add('active');
+// }
+
+// list.forEach((i) =>
+// i.addEventListener('click',active));
+
+let menuToggle = document.querySelector('.menuToggle');
+let header = document.querySelector('header');
+
+menuToggle.addEventListener('click', () => {
+    header.classList.toggle('active');
+})
